4c3564b8bab8f47ebb89b2106ec0522a907b6d4f
fabio alcocer <fabioalcocersejas17@gmail.com>
2023-04-23 21:29:15 -0400

    add ui, animations and tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,123 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TaskList from './components/TaskList'
+import { AnimatePresence, motion } from 'framer-motion'
+import useEmblaCarousel from 'embla-carousel-react'
+
+const TASKS_LISTS: any[] = [
+  {
+    title: 'Work',
+    color: 'bg-red-700',
+    tasks: [
+      {
+        title: 'Fix i18n implementation',
+        completed: true,
+      },
+      {
+        title: 'Company page layout',
+        completed: false,
+      },
+      {
+        title: 'Responsive nits on homepage layout',
+        completed: false,
+      },
+    ],
+  },
+  {
+    title: 'Home',
+    color: 'bg-indigo-700',
+    tasks: [
+      {
+        title: 'Buy laundry soap',
+        completed: true,
+      },
+      {
+        title: 'Do the laundry',
+        completed: true,
+      },
+      {
+        title: 'Read that book',
+        completed: false,
+      },
+      {
+        title: 'Replace kitchen light bulb',
+        completed: false,
+      },
+      {
+        title:
+          "Clean the desktop because I've just dropped some spaghetti code",
+        completed: false,
+      },
+    ],
+  },
+  {
+    title: 'Improvements',
+    color: 'bg-teal-700',
+  },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedTaskList, setSelectedTaskList] = useState<
+    number | null
+  >(null)
+  const [emblaRef] = useEmblaCarousel()
+
+  const closeTaskList = () => {
+    setSelectedTaskList(null)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className='h-screen flex flex-col bg-black text-white'>
+      <div className='p-6'>
+        <h1 className='text-4xl text-center py-16 tracking-tighter'>
+          <span className='font-extrabold'>Clicket</span>{' '}
+          <span className='text-gray-600'>Tasks</span>
+        </h1>
+
+        <div className='text-center'>
+          <button className='border rounded-lg py-2 px-4 block mx-auto mb-2'>
+            +
+          </button>
+          <span className='text-gray-400 font-medium'>Add List</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className={`flex-1 overflow-hidden`} ref={emblaRef}>
+        <div className={`flex items-end gap-4 p-6 h-full`}>
+          {TASKS_LISTS.map((taskList, i) => {
+            return (
+              <TaskList
+                key={taskList.title}
+                title={taskList.title}
+                // color={taskList.color}
+                tasks={taskList.tasks}
+                className={`${
+                  selectedTaskList !== null && selectedTaskList !== i
+                    ? '!opacity-0'
+                    : ''
+                }`}
+                id={`taskList-${i}`}
+                handleClick={() => setSelectedTaskList(i)}
+              />
+            )
+          })}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <AnimatePresence>
+        {selectedTaskList !== null && (
+          <motion.div className='fixed w-screen h-screen z-20 p-4'>
+            <TaskList
+              expanded
+              handleClose={closeTaskList}
+              id={`taskList-${selectedTaskList}`}
+              title={TASKS_LISTS[selectedTaskList].title}
+              color={TASKS_LISTS[selectedTaskList].color}
+              tasks={TASKS_LISTS[selectedTaskList].tasks}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </main>
   )
 }
 
